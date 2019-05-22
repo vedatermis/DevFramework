3bf89f28614e5bbadb80a69fd7fab942846adfaf
Vedat ERMIS <vedatermis@hotmail.com>
2019-05-22 18:33:13 +0300

    validator

---
+++ DevFramework.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using DevFramework.Northwind.Entities.Concrete;
+
+namespace DevFramework.Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+        Product Update(Product product);
+    }
+}
---
+++ DevFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using DevFramework.Core.Aspects.PostSharp;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Business.ValidationRules.FluentValidation;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using DevFramework.Core.Aspects;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager: IProductService
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        [FluentValidationAspect(typeof(ProductValidator))]
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        [FluentValidationAspect(typeof(ProductValidator))]
+
+        public Product Update(Product product)
+        {
+            return _productDal.Update(product);
+        }
+    }
+}
